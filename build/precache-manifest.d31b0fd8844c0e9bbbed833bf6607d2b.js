19bca6e0e9bf2d5c602ad225bb0aad2858465439
ZChanghai <31641419+ZChanghai@users.noreply.github.com>
2018-11-12 16:41:44 +0800

    add first order version
    add first order version

---
+++ build/precache-manifest.d31b0fd8844c0e9bbbed833bf6607d2b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "428c9e8716f3b123bd12c91a8025c05f",
+    "url": "./static/media/iconfont.428c9e87.svg"
+  },
+  {
+    "revision": "346f487a3bedeb5adfcd8babfc636d66",
+    "url": "./static/media/iconfont.346f487a.ttf"
+  },
+  {
+    "revision": "2994b44c80a3b3dd023cd13513bc0f79",
+    "url": "./static/media/iconfont.2994b44c.eot"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "29f75007a5ca3dbde155",
+    "url": "./static/js/main.29f75007.chunk.js"
+  },
+  {
+    "revision": "ca882de5841c1db5bdec",
+    "url": "./static/js/1.ca882de5.chunk.js"
+  },
+  {
+    "revision": "29f75007a5ca3dbde155",
+    "url": "./static/css/main.79a11f87.chunk.css"
+  },
+  {
+    "revision": "ca882de5841c1db5bdec",
+    "url": "./static/css/1.772e0b1a.chunk.css"
+  },
+  {
+    "revision": "a7e3a2efa78b562a2f74e071ba28a2e2",
+    "url": "./index.html"
+  }
+];
